36cb52b3d1c9f7e2167f811d4ea2e3875c0a1fe2
AdrianMitroiu <adrian.mitroiu@unity-solutions.ro>
2019-07-23 16:09:36 +0300

    Initial commit

---
+++ database/migrations/2019_07_23_122118_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username', /*length*/255)->comment = "The username choosen by the user at sign up";
+            $table->string('password', /*length*/255);
+            $table->integer('tenant_id')->unsigned()->default(NULL);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
